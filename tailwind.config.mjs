620417d1d36436c1207857bc8a629860e771460e
Carlos Soto <mrlocked4@gmail.com>
2024-03-12 18:49:51 -0600

    taos added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	// content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+
+
+	// ],
+	content: {
+		relative: true,
+		transform: (content) => content.replace(/taos:/g, ''),
+		files: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+	},
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('taos/plugin')],
+	safelist: [
+		'!duration-[0ms]',
+		'!delay-[0ms]',
+		'html.js :where([class*="taos:"]:not(.taos-init))'
+	]
 }
